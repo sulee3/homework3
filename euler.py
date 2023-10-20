2789fcfaab679500581cb58db7541efcd48ab261
sulee3 <139187794+sulee3@users.noreply.github.com>
2023-10-20 16:53:46 -0500

    Initial commit

---
+++ euler.py
@@ -0,0 +1,9 @@
+"""
+Defintions for problem 0
+"""
+
+import numpy as np
+import scipy.integrate
+from scipy.integrate import DenseOutput
+from scipy.interpolate import interp1d
+from warnings import warn
